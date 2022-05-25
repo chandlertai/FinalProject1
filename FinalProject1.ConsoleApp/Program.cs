dc7ad60641e9074aa9ea2c04731693d121c9f98b
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-05-24 21:23:16 -0400

    Program

--- FinalProject1.ConsoleApp/Program.cs
+++ FinalProject1.ConsoleApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Threading;
+
+namespace FinalProject1
+{
+    class Program
+    {
+        static void Main()
+        {
+            UserInterface _ui = new UserInterface();
+
+            _ui.Run();
+        }
+    }
+}
