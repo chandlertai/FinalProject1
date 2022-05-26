005b895064f85649bee1e29f44109ae71d7b3376
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-05-26 18:40:58 -0400

    program2

--- FinalProject1.ConsoleApp/UserInterface.cs
+++ FinalProject1.ConsoleApp/UserInterface.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FinalProject1.Repository;
 
 namespace FinalProject1.ConsoleApp
 {
     public class UserInterface
     {
-        
+        FinalProjectRepository _repo = new FinalProjectRepository();
+
+        bool isRunning = true;
     }
 }
