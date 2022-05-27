651b2521bf8e79b2630ca9d8c42a8422be928f71
chandlertai <97628440+chandlertai@users.noreply.github.com>
2022-05-26 21:03:17 -0400

    UI

--- FinalProject1.ConsoleApp/UserInterface.cs
+++ FinalProject1.ConsoleApp/UserInterface.cs
@@ -11,5 +11,54 @@ namespace FinalProject1.ConsoleApp
         FinalProjectRepository _repo = new FinalProjectRepository();
 
         bool isRunning = true;
+
+        public void Run()
+        {
+            _repo.SeedClientData();
+
+            while (isRunning)
+            {
+                _console.PrintMainMenu();
+                string userInput = _console.GetUserInput();
+
+                UserInputPortal(userInput);
+            }
+        }
+
+        private void UserInputPortal(string inputFromUser)
+        {
+            switch (inputFromUser)
+            {
+                case "1":
+                    CreateClaimId();
+                    break;
+                case "2":
+                    ClaimType();
+                    break;
+                case "3":
+                    ClaimDescription();
+                    break;
+                case "4":
+                    ClaimAmount();
+                    break;
+                case "5":
+                    DateOfIncident();
+                    break;
+                case "6":
+                    DateOfClaim();
+                    break;
+                case "7":
+                    IsValid();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void CreateClaimId()
+        {
+            _console.EnterAnId();
+            string claimId = _console.GetUserInput();
+        }
     }
 }
